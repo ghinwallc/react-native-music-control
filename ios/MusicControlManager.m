3ee406f93033ef008e9920adcbbc6f881badcbcc
Rafal Augustyniak <rafal@macoscope.net>
2016-06-03 10:56:07 +0200

    GH-54: Remove `enableBackgroundMode` method

--- ios/MusicControlManager.m
+++ ios/MusicControlManager.m
@@ -157,14 +157,6 @@ RCT_EXPORT_METHOD(enableControl:(NSString *) controlName enabled:(BOOL) enabled 
     }
 }
 
-/* We need to set the category to allow remote control etc... */
-
-RCT_EXPORT_METHOD(enableBackgroundMode:(BOOL) enabled){
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    [session setCategory: AVAudioSessionCategoryPlayback error: nil];
-    [session setActive: enabled error: nil];
-}
-
 #pragma mark internal
 
 - (void) toggleHandler:(MPRemoteCommand *) command withSelector:(SEL) selector enabled:(BOOL) enabled {
